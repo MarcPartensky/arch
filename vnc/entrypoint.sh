d0388c236060204a9bc7ba51e9ea40d684d16ff2
Marc Partensky <marc.partensky@gmail.com>
2022-06-21 02:33:56 +0200

    supervisord as job

--- vnc/entrypoint.sh
+++ vnc/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/sbin/zsh
 
-/usr/bin/supervisord -c /etc/supervisor.d/supervisord.ini
+/usr/bin/supervisord -c /etc/supervisor.d/supervisord.ini &
 # picom -b
 eval "sleep 10 && feh --bg-fill wallpapers/1.jpg" &
 alacritty
