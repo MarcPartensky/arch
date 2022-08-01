6d63cb21555d0b6d65fc2c4092baa435bfbf5f47
Marc Partensky <marc.partensky@gmail.com>
2022-08-01 17:30:29 +0200

    log in tmp snif

--- vnc/entrypoint.sh
+++ vnc/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export WALLPAPERS_PATH=/home/marc/wallpapers
-supervisord -c /opt/arch/supervisord.conf -l supervisord.log
+supervisord -c /opt/arch/supervisord.conf -l /tmp/supervisord.log
