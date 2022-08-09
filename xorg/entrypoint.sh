1157533056628634bf4268277c39d4f71831d517
Marc Partensky <marc.partensky@gmail.com>
2022-08-09 20:29:57 +0200

    conf

--- xorg/entrypoint.sh
+++ xorg/entrypoint.sh
@@ -3,4 +3,4 @@
 export WALLPAPERS_PATH=/home/marc/wallpapers
 # export DISPLAY=:0
 # Xephyr :1 -extension MIT-SHM
-/usr/bin/supervisord -c /etc/supervisor.d/supervisord.ini
+/usr/bin/supervisord -c /etc/supervisor.d/supervisord.conf
